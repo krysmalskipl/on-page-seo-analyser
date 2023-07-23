af6a43042083920f32591cb8a37da8f945b6b4ad
krysmalskipl <jakub16@gmail.com>
2023-07-24 01:30:11 +0200

    added popular services link section

--- popup.js
+++ popup.js
@@ -157,3 +157,15 @@ let copyHeadersButton = document.getElementById('copyHeaders');
 copyHeadersButton.addEventListener('click', copyHeadersToClipboard);
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByClassName('external-link');
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({ active: true, url: location });
+            };
+        })();
+    }
+});
